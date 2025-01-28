2ea5564cece74d22b1d4eb8fded06f4a67c76ded
YagorYafimau <efimov1711@gmail.com>
2025-01-28 13:56:12 +0300

    ignore failure test

--- tests/regression/login/login-failure.test.ts
+++ tests/regression/login/login-failure.test.ts
@@ -10,15 +10,16 @@
 //   const successMessage = await page.locator('text=Welcome');
 //   await expect(successMessage).toBeVisible();
 // });
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../../page-objects/login-page';
 
-test.skip('failure login test', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goToLoginPage();
+// import { test, expect } from '@playwright/test';
+// import { LoginPage } from '../../../page-objects/login-page';
 
-  await loginPage.login(process.env.LOGIN!, process.env.PASSWORD1!);
+// test.skip('failure login test', async ({ page }) => {
+//   const loginPage = new LoginPage(page);
+//   await loginPage.goToLoginPage();
 
-  const successMessage = await page.locator('text=Welcome');
-  await expect(successMessage).toBeVisible();
-});
+//   await loginPage.login(process.env.LOGIN!, process.env.PASSWORD1!);
+
+//   const successMessage = await page.locator('text=Welcome');
+//   await expect(successMessage).toBeVisible();
+// });
